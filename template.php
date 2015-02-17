edb2d50dc95f838b5e70965c39bd8b149cf05a6b
Michael Collins <mscoll85@gmail.com>
2015-02-17 16:43:10 -0500

    ODL theme repo init

---
+++ template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function foundation_odl_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function foundation_odl_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function foundation_odl_preprocess_node(&$variables) {
+}
