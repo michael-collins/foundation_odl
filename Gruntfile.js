585b1e7e40586dcfcbad57cd6357f06a16f7e0c7
Michael Collins <mscoll85@gmail.com>
2015-03-20 21:11:58 -0400

    Added video header bg

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,6 +113,52 @@ module.exports = function(grunt) {
       }
     },
 
+    responsive_videos: {
+      myTask: {
+        options: {
+          sizes:[{
+            name: "small",
+            width: 640,
+            filter: '',
+            poster: true
+          },{
+            name: "medium",
+            width: 960,
+            filter: '',
+            poster: true
+          }],
+          encodes:[{
+            webm: [
+                {'-vcodec': 'libvpx'},
+                {'-acodec': 'libvorbis'},
+                {'-q:a': '100'},
+                {'-quality': 'good'},
+                {'-cpu-used': '0'},
+                {'-b:v': '500k'},
+                {'-qmax': '42'},
+                {'-maxrate': '500k'},
+                {'-bufsize': '1000k'},
+                {'-threads': '0'}
+            ],
+            mp4: [
+                {'-vcodec':'libx264'},
+                {'-acodec': 'libfaac'},
+                {'-pix_fmt': 'yuv420p'},
+                {'-q:v': '4'},
+                {'-q:a': '100'},
+                {'-threads': '0'}
+            ]
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['video/**.{mov,mp4}'],
+          cwd: 'assets/',
+          dest: 'tmp/'
+        }]
+      }
+    },
+
     watch: {
       grunt: { files: ['Gruntfile.js'] },
 
@@ -136,7 +182,10 @@ module.exports = function(grunt) {
   });
   grunt.loadNpmTasks('grunt-svgmin');
   grunt.loadNpmTasks('grunt-grunticon');
+  grunt.loadNpmTasks('grunt-responsive-videos');
 
+  grunt.registerTask('video', ['responsive_videos:myTask']);
   grunt.registerTask('build', ['jshint','uglify','svgmin','grunticon:myIcons','sass']);
+  grunt.registerTask('superbuild', ['build', 'video', 'watch']);
   grunt.registerTask('default', ['build', 'watch']);
 };
